b465f3b1eb4bf42af208bf767e50f91a4216d3ba
phella <philopateernabil2021@gmail.com>
2020-03-03 07:54:36 -0800

    build tracker nodes

--- Files/master_alive.py
+++ Files/master_alive.py
@@ -1,11 +1,12 @@
 import sys
 import zmq
+from multiprocessing import Process, Value, Array
 from utility import log 
 
 port = "5556"
 n = int(sys.argv[1]) # Number of data keepers
 current = n     # Number of alive data keepers
-status = [True]*n  # Status of data keepers
+status = Array('i', [1]*n)  # Status of data keepers
 active =  {0}      # Every second collect data keepers in set
 active.clear()
 counter = 0      # Every second count data keepers
@@ -13,9 +14,27 @@ ips = []        # List of data keepers id
 ports = []      # List of ports
 storage = [1024]*n     # List of free storage on each data keeper
 
+
 for i in range(0 , 2*n - 1 , 2):
     ips.append(sys.argv[2+i])
     ports.append(sys.argv[3+i])
+
+def tracker(dummy,status_table):
+    for i in range(len(status_table)):
+        print(status_table[i])
+
+
+
+def build_tracker():
+    p1 = Process(target = tracker , args = (1,status) )
+    p2 = Process(target = tracker , args = (1,status) )
+    p3 = Process(target = tracker , args = (1,status) )
+    p1.start()
+    p2.start()
+    p3.start()
+
+
+build_tracker()
 
 
 # Socket to talk to server
@@ -36,16 +55,15 @@ while(True):
     id = int(id)
     active.add(id)
     storage[id - 1 ] = int(size)
-    if(status[ id - 1 ] == False):         # Dead data keeper return back alive
+    if(status[ id - 1 ] == 0):         # Dead data keeper return back alive
         current += 1
     counter += 1
     if( counter ==  current ):                # 1 sec completed
         if(len(active) != current):           # Replicated data keeper means 1 data keeper is dead
-            status = [False] * n
+            status = [0] * n
             current = len(active)             # New current number
             for i in range(0 , current):
-                status[active.pop() - 1 ] = True    # Mark active data keepers 
+                status[active.pop() - 1 ] = 1    # Mark active data keepers 
         counter = 0                                 #init counters
         active.clear()
     
- 
