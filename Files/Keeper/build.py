bf8eff530b4ca0bec52aea0385907bcea1e8bdf8
a7med7amdy <engahmedhamdy1212@gmail.com>
2020-03-04 21:55:11 +0200

    new commit for Data keeper and client

--- Files/Keeper/build.py
+++ Files/Keeper/build.py
@@ -4,10 +4,10 @@ import subprocess
 import os
 import sys
 from multiprocessing import Process
-sys.path.append('../')
-from utility import remove_log
-from Keeper.keeper import keeper
-from Keeper.isAlive import alive 
+sys.path.insert(0, "../../Files/")
+from Files.utility import remove_log
+from Files.Keeper.data_keeper import Keeper
+from Files.Keeper.isAlive import alive 
 
 
 remove_log()
@@ -15,7 +15,7 @@ sys.path.append('Keeper/')
 n = 3
 # run same data keeper many times
 for i in range(0,n):
-    p0 = Process(target = keeper , args = ( str(i) , str(5400+i)) )
+    p0 = Process(target = Keeper , args = ( str(i) , str(5400+i)) )
     p0.start()
 
 
