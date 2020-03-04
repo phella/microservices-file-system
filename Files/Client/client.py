e3848a2a10800fde57bdf0484f36c0ac91b2cbc2
a7med7amdy <engahmedhamdy1212@gmail.com>
2020-03-05 01:27:50 +0200

    adding client and DataKeeper

---
+++ Files/Client/client.py
@@ -0,0 +1,43 @@
+import zmq
+import sys
+import random
+import time
+import os
+import cv2 
+import up_down
+
+
+port = "5559"
+context = zmq.Context()
+print ("Connecting to server...")
+socket = context.socket(zmq.REQ)
+socket.connect ("tcp://localhost:%s" % port)
+client_id = random.randrange(1,10005)
+
+
+context = zmq.Context()
+socketDk = context.socket(zmq.REQ)
+
+# i need to upload/download a video ask tracker
+def askTracker(type):
+    socket.send_pyobj ({"type":type})
+    #  Get the reply of the DK and port no of it to communicate. 
+    message = socket.recv_pyobj()
+    return message
+
+def makeConnections(ports,ips):
+    for i in range (len(ports)):
+        socketDk.connect("tcp://"+ips[i]+":%s" % port[i])
+
+def main():
+    type= sys.argv[2]  #take upload or download
+    file= sys.argv[3]  #take upload or download
+    message=askTracker(type)
+    ports=message["ports"]
+    ips=message["ips"]
+    makeConnections(ports,ips)
+    if type=="upload":
+        print(up_down.upload(socketDk,file))
+    else:
+        up_down.download(socketDk,file)
+
---
+++ Files/Keeper/data_keeper.py
@@ -0,0 +1,28 @@
+import zmq
+import random
+import sys
+import time
+
+sys.path.insert(0, "../../Files/")
+import Files.up_down as up_down
+
+#pair connection to serve client upload/download
+def Keeper(id,port2):
+    context = zmq.Context()
+    socket2 = context.socket(zmq.REP)
+    socket2.bind("tcp://*:%s" % port2)
+
+    socket2.send("Server message to client3")
+    message = socket2.recv_pyobj()
+    if(message["type"]=="download"):  #download
+        up_down.upload(socket2,message["file"])
+    else : #upload
+        up_down.download(socket2,message["file"])
+    time.sleep(1)
+
+    #tell master tracker i've finished U/D file...
+    socket2.send_pyopbj({"msg":"successfull_work","file" : None,"type":message["type"]})
+
+
+##the master should inform the client with successfull work and save file in LUT
+###########################################################
