b4c9c4354ec545267c5b148ac39165c3778dce38
Marc Nagui <45571009+MarcNagui@users.noreply.github.com>
2020-03-07 02:36:51 +0200

    dummyClient errors modified

--- Files/Keeper/dummyClient.py
+++ Files/Keeper/dummyClient.py
@@ -1,14 +1,24 @@
 import zmq
 import sys
 import time
+sys.path.insert(1, '../../Files')
+
 import up_down
 def dummyClient(port):
     context = zmq.Context()
     socket = context.socket(zmq.PULL)
     socket.bind("tcp://*:%s" % port)
+    context = zmq.Context()
+    socketDk = context.socket(zmq.REQ)
+
     while True:
-        socket.recv_pyobj()
+        message=socket.recv_pyobj()
         ports=message["ports"]
         ips=message["ips"]
-        makeConnections(ports,ips)
+        socketDk=makeConnections(ports, ips)
         up_down.upload(socketDk,message["filename"])
+
+
+    def makeConnections(ports, ips):
+        for i in range(len(ports)):
+            socketDk.connect("tcp://"+ips[i]+":%s" % port[i])
