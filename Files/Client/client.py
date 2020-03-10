dd7458f55b48f4d115fef1a6255435e3deed9333
phella <philopateernabil2021@gmail.com>
2020-03-10 09:10:47 -0700

    fix interface between master and client

--- Files/Client/client.py
+++ Files/Client/client.py
@@ -5,13 +5,15 @@ import time
 import os
 import cv2 
 import up_down
+from utility import log , remove_log
 
-
-port = "5559"
+remove_log()
+port = 5556
 context = zmq.Context()
 print ("Connecting to server...")
 socket = context.socket(zmq.REQ)
-socket.connect ("tcp://localhost:%s" % port)
+for i in range(3):
+    socket.connect ("tcp://localhost:%s" % str(port + i))
 client_id = random.randrange(1,10005)
 
 
@@ -19,25 +21,26 @@ context = zmq.Context()
 socketDk = context.socket(zmq.REQ)
 
 # i need to upload/download a video ask tracker
-def askTracker(type):
-    socket.send_pyobj ({"type":type})
+def askTracker(type , filename):
+    socket.send_pyobj ({"type":type , "file":filename})
+    log(" Request of type" + type + " sent")
     #  Get the reply of the DK and port no of it to communicate. 
     message = socket.recv_pyobj()
+    log(" Response recieved with ip" + message["ips"][0] + " port number " +message["ports"][0])
     return message
 
 def makeConnections(ports,ips):
     for i in range (len(ports)):
-        socketDk.connect("tcp://"+ips[i]+":%s" % port[i])
+        socketDk.connect(ips[i] + ":%s" % ports[i])
 
-def main():
-    type= sys.argv[2]  #take upload or download
-    file= sys.argv[3]  #take upload or download
-    message=askTracker(type)
-    ports=message["ports"]
-    ips=message["ips"]
+def main(type , filename):
+    message = askTracker(type ,filename)
+    ports = message["ports"]
+    ips = message["ips"]
     makeConnections(ports,ips)
-    if type=="upload":
-        print(up_down.upload(socketDk,file))
+    if type == "upload":
+        up_down.upload(socketDk,filename)
     else:
-        up_down.download(socketDk,file)
+        up_down.download(socketDk,filename)
 
+main(sys.argv[1] ,  sys.argv[2] )
---
+++ Files/Client/up_down.py
@@ -0,0 +1,28 @@
+import zmq
+import sys
+import random
+import time
+import os
+import cv2 
+
+def upload (socketDk,file):
+    cap =cv2.cv2.VideoCapture(file) #path of the file
+    fps= cap.get(cv2.cv2.CV_CAP_PROP_FPS)
+    frame_width = int (cap.get(3))
+    frame_height = int (cap.get(4)) 
+    success,image = cap.read()
+    socketDk.send_pyobj({"type":"upload","file":file,"protocol":[fps,frame_width,frame_height]})
+    while(success):
+        socketDk.send_pyobj({"frame":image})
+        success,image = cap.read()
+    return socketDk.recv_pyobj()
+
+def download (socketDk,file):
+    socketDk.send_pyobj({"type":"download","file":file})
+    ret=socketDk.recv_pyobj()
+    out= cv2.cv2.VideoWriter(file,cv2.cv2.VideoWriter_fourcc(*'MP4V'),ret["protocol"][0],(ret["protocol"][1],ret["protocol"][2]))
+    while (True):
+        frame=socketDk.recv_pyobj()
+        if frame["frame"] == "sucess":
+            break
+        out.write(frame["frame"])
