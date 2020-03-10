24497d62b2d07c7eb43f530952b3da149597395c
phella <philopateernabil2021@gmail.com>
2020-03-10 07:57:27 -0700

    fix syntax errors in master

--- Files/Master/build.py
+++ Files/Master/build.py
@@ -1,19 +1,20 @@
 import sys
-sys.path.append('../')
+#sys.path.insert( 1 , '/../')
 import zmq
 from multiprocessing import Process, Array , Manager
+from tracker import tracker
+from isAlive import alive
+from replica import replica
+from events import update_table
+
 from utility import log , remove_log
-from Master.isALive import alive
-from Master.tracker import tracker
-from Master.replica import replica
-from Keeper.events import update_table
 
 no_process = 3
 port_base = 5556
 no_keepers = 1
-status = Array('i', [1]*n)  # Status of data keepers
+status = Array('i', [1]*no_keepers)  # Status of data keepers
 ips = [
-    "tcp://127.0.0.1:
+    "tcp://127.0.0.1:"
 ]
 
 if __name__ == '__main__':
@@ -24,7 +25,7 @@ if __name__ == '__main__':
             free_ports[i] = [ 5700 , 5701 , 5702 ] 
         p0 = Process(target = alive , args = ( no_keepers , ips , status , lookup_table , free_ports) )
         for i in range (no_process):
-            p1 = Process(target = tracker , args = (i , ips , str( port_base + i )  ,  status , lookup_table , free_ports) )
+            p1 = Process(target = tracker , args = (i , no_keepers , ips , str( port_base + i )  ,  status , lookup_table , free_ports) )
             p1.start()
             p1.join()
         p1 = Process( target = replica , args = (no_process,status,lookup_table,free_ports))
