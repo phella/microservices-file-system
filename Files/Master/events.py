c59306e43fc19a3cb669d2ccbeb4dfe8d301ae2e
phella <philopateernabil2021@gmail.com>
2020-03-15 11:40:14 -0700

    change variable id name

--- Files/Master/events.py
+++ Files/Master/events.py
@@ -16,15 +16,15 @@ def update_table( port , ips , free_ports , lookup_table ):
     socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
     while True:
         string = socket.recv_string()
-        topic , id , free_port , filename = string.split()
-        log(" Recived from id "+ id +" filename " + filename + " port free " + free_port)
-        temp = free_ports[int(id)]
+        topic , keeper_id , free_port , filename = string.split()
+        log(" Recived from keeper_id "+ keeper_id +" filename " + filename + " port free " + free_port)
+        temp = free_ports[int(keeper_id)]
         temp.append(free_port)
-        free_ports[int(id)] = temp 
+        free_ports[int(keeper_id)] = temp 
         if filename in lookup_table :
             temp = lookup_table[filename]
-            temp.append(id)
+            temp.append(keeper_id)
             lookup_table[filename] = temp
         else :
-            lookup_table[filename] = [id]
-
+            lookup_table[filename] = [keeper_id]
+        print(lookup_table)
