7b58e8e07a0167e994a00b652f58b440725a8787
phella <philopateernabil2021@gmail.com>
2020-03-16 08:36:26 -0700

    refactor isAlive logic

--- Files/Master/build.py
+++ Files/Master/build.py
@@ -1,34 +1,36 @@
 import sys
-#sys.path.insert( 1 , '/../')
 import zmq
 from multiprocessing import Process, Array , Manager
 from tracker import tracker
 from isAlive import alive
 from replica import replica
 from events import update_table
+
 sys.path.append('../')
 from utility import log , remove_log
 
+# Remove old logs
 remove_log()
-no_process = 3
-port_base = 5556
-no_keepers = 2
-status = Array('i', [1]*no_keepers)  # Status of data keepers
+
+no_processes = 3   # No of tracker processes
+port_base = 5556   # Port numbers base of tracker nodes
+no_keepers = 2    
+status = Array('i', [1]*no_keepers)  # Status of data keepers alive or dead
 ips = [
-    "tcp://127.0.0.1:",
+    "tcp://127.0.0.1:",         # Array of data keepers arranged with keepers ids 
     "tcp://192.168.43.79:"
 ]
 
 if __name__ == '__main__':
     with Manager() as manager:
-        lookup_table = manager.dict()
-        free_ports = manager.dict()
+        lookup_table = manager.dict()   # {'filename' : [kepeer_ids] }
+        free_ports = manager.dict()     # { keeper_id : [free_ports] }
         log("Shared memory created successefully")
         for i in range(3) : 
             free_ports[i] = [ 5700 , 5701 , 5702 ] 
         p0 = Process(target = alive , args = ( no_keepers , ips , status , lookup_table , free_ports) )
         p0.start()
-        for i in range (no_process):
+        for i in range (no_processes):
             p1 = Process(target = tracker , args = (i , no_keepers , ips , str( port_base + i )  ,  status , lookup_table , free_ports) )
             p1.start()
         p1 = Process( target = replica , args = (no_keepers , 2 , status,lookup_table , free_ports , ips))
--- Files/Master/isAlive.py
+++ Files/Master/isAlive.py
@@ -2,42 +2,47 @@ import sys
 import zmq
 import signal
 from multiprocessing import Process, Array , Manager
-from utility import log ,listToString
-sys.path.append('../')
-from utility import log , remove_log
+from functools import partial
 
-keepers = 0
+sys.path.append('../')
+from utility import log , remove_log , listToString
+
+keepers = 0 # Number of keepers
+current = 0 # Number of alive keepers
+
 def alive( no_keepers , ips , status , lookup_table , free_ports):
+    global current
     current = no_keepers     # Number of alive data keepers
     global keepers
     keepers = no_keepers
     active =  {0}      # Every second collect data keepers in set
     active.clear()
     counter = 0      # Every second count data keepers
-    signal.signal(signal.SIGALRM, handler)
+    signal.signal(signal.SIGALRM, partial(handler ,status))
 
     # Socket to talk to server
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
 
     for i in range(0 , no_keepers):
-        socket.connect ( ips[i] + "6000" )
+        socket.connect ( ips[i] + "6000" )  # Connect to isAlive of keepers 
 
     topicfilter = "isALive"
     socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
     
     while(True): 
-        signal.alarm(2)
+        signal.alarm(2)         # After 2 seconds all Processes are dead
         state = socket.recv_string()
         topic , id = state.split()
-        #log("Data keeper " + id + " alive")
         id = int(id)
-        active.add(id)
+        active.add( id )
         if(status[ id ] == 0):         # Dead data keeper return back alive
             current += 1
+            status[id] = 1
+            log(" Data keeper is back")
         counter += 1
         if( counter ==  current ):                # 1 sec completed
-            if(len(active) != current):           # Replicated data keeper means 1 data keeper is dead
+            if( len(active) != current ):           # Replicated data keeper means 1 data keeper is dead
                 log(" Detected dead data keeper")
                 for i in range(no_keepers):
                     status[i] = 0
@@ -49,7 +54,12 @@ def alive( no_keepers , ips , status , lookup_table , free_ports):
             log(listToString(status))
 
 
-def handler(signum, frame):
+def handler(status ,signum, frame):
     signal.alarm(2)
-    status = [0]*keepers
+    log(" All data keepers are dead")
+    global keepers
+    global current
+    for i in range(keepers):
+        status[i] = 0
+    current = 0
     log(listToString(status))
